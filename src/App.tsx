fef5b88a11185f7eb196d15fa0c74ea604ecb802
Naveen <naveenbs135@gmail.com>
2025-07-04 14:35:22 +0530

    Minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import { SignedIn, SignInButton, SignedOut } from "@clerk/clerk-react";
-import Footer from "./components/Footer"
-import Manager from "./components/ui/Manager"
-import Navbar from "./components/ui/Navbar"
-import { Toaster } from "./components/ui/sonner"
-import { AlertDialog } from "@radix-ui/react-alert-dialog";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
 import { Button } from "./components/ui/button";
+import Navbar from "./components/ui/Navbar";
+import Manager from "./components/ui/Manager";
+import Footer from "./components/Footer";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
-
   return (
     <>
-
       <SignedIn>
         <Toaster position="top-center" />
-        <AlertDialog />
         <Navbar />
         <Manager />
         <Footer />
       </SignedIn>
       <SignedOut>
         <SignInButton mode="modal">
-          <Button variant="default" className="font-semibold cursor-pointer">
-            Sign In
-          </Button>
+          <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,22 +1,20 @@
-import { SignedIn, UserButton } from '@clerk/clerk-react';
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
   return (
     <div className='h-15 max-w-full bg-slate-800 text-white flex justify-between items-center px-2'>
-      <div className='scroll-m-20 m-auto sm:m-0 text-center text-2xl font-extrabold tracking-tight text-balance flex items-center'>
+      <div className='text-2xl font-extrabold flex items-center'>
         <a href="/"><video width={65} height={65} autoPlay muted loop src="/Animation - 1751560663790.webm" /></a>
-        <a href="/"><span>Safe<b className="text-green-500">X</b></span></a>
+        <a href="/">Safe<b className="text-green-500">X</b></a>
       </div>
 
-      <div className="flex justify-end items-center shadow-md">
-        <header>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </header>
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
